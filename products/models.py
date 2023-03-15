5dd19b7e7ac9ecae9df0cb3fe27aadf511b3f61e
MFLind <Madde@quaggatech.com>
2023-03-15 08:37:52 +0000

    Adding Products Part 3

--- products/models.py
+++ products/models.py
@@ -12,8 +12,8 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-        def get_friendly_name(self):
-            return self.friendly_name
+    def get_friendly_name(self):
+        return self.friendly_name
 
 
 class Product(models.Model):
@@ -21,6 +21,7 @@ class Product(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
